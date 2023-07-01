d4ff459f6a932b85043d948dcfcff13d707fd009
Apurbo73 <apurbodebnath895@gmail.com>
2023-07-01 19:23:33 +0600

    login page ui done

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import cart from '../images/cart.jpg';
 const Login = () => {
   return (
-    <div >
+    <div>
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
           <Link to="/" className="navbar-brand text-warning" href="#">
@@ -41,36 +41,44 @@ const Login = () => {
           </div>
         </div>
       </nav>
-      <div className="pt-5">
-        <form className="mx-auto mt-5 w-50 shadow-lg p-3 mb-5 bg-body rounded">
-          <h3 className="text-center">Enter Your Detail</h3>
-          <div className="mb-3">
-            <input
-              type="email"
-              className="form-control"
-              id="exampleInputEmail1"
-              aria-describedby="emailHelp"
-              placeholder="Enter your email"
-            />
-          </div>
-          <div className="mb-3">
-            <input
-              type="password"
-              className="form-control"
-              id="exampleInputPassword1"
-              placeholder="Enter your password"
-            />
-          </div>
-          <button type="submit" className="btn btn-warning w-100">
-            Login
-          </button>
-          <div className="d-flex">
-            <p className="m-1">Not registered?</p>
-            <Link className="m-1" to="/sign-up">
-              sign up
-            </Link>
-          </div>
-        </form>
+      <div className="pt-5 w-50 container d-flex mx-auto text-center ">
+        <div style={{ height: 300 }} className="w-50 mt-5 bg-warning">
+          <h4 className="mt-5 pt-5">Mithila's Outlet</h4>
+          <img className="rounded-circle" style={{height:100}} src={cart} alt="" />
+          <p className=" pt-4">The best online shop near you..</p>
+        </div>
+
+        <div className=" mt-5 w-50 shadow-lg p-3 mb-5 bg-body rounded ">
+          <form className=" pb-5">
+            <h3 className="text-center">Sign in</h3>
+            <div className="mb-3">
+              <input
+                type="email"
+                className="form-control"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+                placeholder="Enter your email"
+              />
+            </div>
+            <div className="mb-3">
+              <input
+                type="password"
+                className="form-control"
+                id="exampleInputPassword1"
+                placeholder="Enter your password"
+              />
+            </div>
+            <button type="submit" className="btn btn-warning w-100 ">
+              Submit
+            </button>
+            <div className="d-flex">
+              <p className="m-1">Not registered?</p>
+              <Link className="m-1" to="/sign-up">
+                sign up
+              </Link>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
