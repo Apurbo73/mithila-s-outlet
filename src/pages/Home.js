90f92e674e908a57f4a27cbe75e68cc56d74284b
Apurbo73 <apurbodebnath895@gmail.com>
2023-07-04 22:27:42 +0600

    ui update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -55,7 +55,7 @@ const Home = ({ allData }) => {
     <div>
       <Navbar onSearch={handleSearch} />
       {/* simple  design starts*/}
-      <div className="d-none ">
+      <div className="d-none d-md-block ">
         <div className="mt-5 pt-5 d-flex  ">
           <div className=" ">
             <img className="image" src={image1} alt="" />
