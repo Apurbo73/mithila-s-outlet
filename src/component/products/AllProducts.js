18d3026262339b2a98ac4894914134245098f9d0
Apurbo73 <apurbodebnath895@gmail.com>
2023-07-07 04:14:45 +0600

    slight changes

--- src/component/products/AllProducts.js
+++ src/component/products/AllProducts.js
@@ -3,9 +3,11 @@ import Product from "./Product";
 
 const AllProducts = ({ allProducts }) => {
   return (
-    <div className="d-flex flex-wrap mt-3 ">
+<div className="">
+<div className=" mt-3 row   ">
       {allProducts && allProducts.map(product => <Product key={product.id} product={product} />)}
     </div>
+</div>
   );
 };
 
--- src/component/products/Product.js
+++ src/component/products/Product.js
@@ -2,35 +2,65 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Product = ({ product }) => {
-  const { name, price, seller, image, category,id } = product;
+  const { name, price, seller, image, category, id } = product;
   return (
-    <div className="mx-auto ">
-      <div
-        className="card image-card  shadow-lg p-3 mb-5  rounded-25 "
-        style={{ width: "15rem", height: 400 }}
-      >
-        <img
-          style={{ height: 240 }}
-          src={image}
-          className="card-img-top p-3 image   rounded"
-          alt="..."
-        />
-        <div className="card-body ">
-          <h5 className="card-title">
-            {name}
-          </h5>
-          <p className="card-text">
-            Price: {price} Taka
-          </p>
-          {/* <p className="card-text">
-            Category: {category}
-          </p> */}
-          {/* <p className="card-text">
-            Seller: {seller}
-          </p> */}
-          <Link to={`/products/detail/${id}`} className="btn btn-dark w-100">
-            See detail
-          </Link>
+    // <div className="col-12 col-md-12 col-lg-2 ">
+    //   <div
+    //     className="card image-card  shadow-lg p-3 mb-5 "
+    //     style={{ width: "15rem", height: 400 }}
+    //   >
+    //     <img
+    //       style={{ height: 240 }}
+    //       src={image}
+    //       className="card-img-top p-3 image   rounded"
+    //       alt="..."
+    //     />
+    //     <div className="card-body ">
+    //       <h5 className="card-title">
+    //         {name}
+    //       </h5>
+    //       <p className="card-text">
+    //         Price: {price} Taka
+    //       </p>
+    //       {/* <p className="card-text">
+    //         Category: {category}
+    //       </p> */}
+    //       {/* <p className="card-text">
+    //         Seller: {seller}
+    //       </p> */}
+    //       <Link to={`/products/detail/${id}`} className="btn btn-dark w-100">
+    //         See detail
+    //       </Link>
+    //     </div>
+    //   </div>
+    // </div>
+
+    <div className="col-12 col-md-3 col-lg-2">
+      <div className="card text-center text-dark  pb-2 mt-3">
+        <div className="card-body">
+          <img
+            style={{ height: 240 }}
+            src={image}
+            className="card-img-top p-3 image   rounded"
+            alt="..."
+          />
+          <div className="card-body ">
+            <h5 className="card-title">
+              {name}
+            </h5>
+            <p className="card-text">
+              Price: {price} Taka
+            </p>
+            <p className="card-text">
+              Category: {category}
+            </p>
+            <p className="card-text">
+              Seller: {seller}
+            </p>
+            <Link to={`/products/detail/${id}`} className="btn btn-dark w-100">
+              See detail
+            </Link>
+          </div>
         </div>
       </div>
     </div>
